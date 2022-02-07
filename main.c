caf5b6f674bed0354b3e1027271b8de0e864419d
Mathieu Gouin <mgouin@gmail.com>
2022-02-07 12:55:02 -0500

    working example with a texture

--- main.c
+++ main.c
@@ -10,6 +10,47 @@
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 800
 
+/*  Create checkerboard texture  */
+#define checkImageWidth 64
+#define checkImageHeight 64
+static GLubyte checkImage[checkImageHeight][checkImageWidth][4];
+static GLuint texName;
+
+void makeCheckImage(void)
+{
+   int i, j, c;
+    
+   for (i = 0; i < checkImageHeight; i++) {
+      for (j = 0; j < checkImageWidth; j++) {
+         c = ((((i&0x8)==0)^((j&0x8))==0))*255;
+         checkImage[i][j][0] = (GLubyte) c;
+         checkImage[i][j][1] = (GLubyte) c;
+         checkImage[i][j][2] = (GLubyte) c;
+         checkImage[i][j][3] = (GLubyte) 255;
+      }
+   }
+}
+
+void init(void)
+{    
+   glClearColor (0.0, 0.0, 0.0, 0.0);
+
+   makeCheckImage();
+   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+   glGenTextures(1, &texName);
+   glBindTexture(GL_TEXTURE_2D, texName);
+
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth, 
+                checkImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, 
+                checkImage);
+}
+
 void keyboard(unsigned char key, int x, int y)
 {
   switch (key)
@@ -31,13 +72,24 @@ void display(void)
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glColor3f(0.0, 1.0, 0.0);
-    glBegin(GL_POLYGON);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, texName);
+
+    //glColor3f(0.0, 1.0, 0.0);
+    glBegin(GL_QUADS);
+        glTexCoord2f(0.0, 0.0);
         glVertex3f(10.0, 10.0, 0.0);
+
+        glTexCoord2f(0.0, 1.0);
         glVertex3f(10.0, WINDOW_HEIGHT - 10, 0.0);
+
+        glTexCoord2f(1.0, 1.0);
         glVertex3f(WINDOW_WIDTH - 10, WINDOW_HEIGHT - 10, 0.0);
+
+        glTexCoord2f(1.0, 0.0);
         glVertex3f(WINDOW_WIDTH - 10, 10.0, 0.0);
     glEnd();
+    glDisable(GL_TEXTURE_2D);
 
     glutSwapBuffers();
 }
@@ -67,6 +119,8 @@ int main(int argc, char** argv)
     glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("OpenGL");
+
+    init();
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape); 
