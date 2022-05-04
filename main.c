478010904c9fc331b8d73c61c1858bda30331762
Mathieu Gouin <mgouin@gmail.com>
2022-05-04 14:14:43 -0400

    Update main.c

--- main.c
+++ main.c
@@ -21,9 +21,11 @@ static GLuint texName;
 void makeCheckImage(void)
 {
     int i, j, c;
-    
-    for (i = 0; i < checkImageHeight; i++) {
-        for (j = 0; j < checkImageWidth; j++) {
+
+    for (i = 0; i < checkImageHeight; i++)
+    {
+        for (j = 0; j < checkImageWidth; j++)
+        {
             c = NB_BAND * i;
             c /= checkImageHeight;  // 0 - 3
             c *= (255 / (NB_BAND - 1));
@@ -42,38 +44,39 @@ void makeCheckImage(void)
 }
 
 void init(void)
-{    
-   glClearColor (0.0, 0.0, 0.0, 0.0);
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
 
-   makeCheckImage();
-   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    makeCheckImage();
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-   glGenTextures(1, &texName);
-   glBindTexture(GL_TEXTURE_2D, texName);
+    glGenTextures(1, &texName);
+    glBindTexture(GL_TEXTURE_2D, texName);
 
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
-   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth, 
-                checkImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, 
-                checkImage);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth,
+                 checkImageHeight,
+                 0, GL_RGBA, GL_UNSIGNED_BYTE,
+                 checkImage);
 
-   glShadeModel(GL_SMOOTH);
+    glShadeModel(GL_SMOOTH);
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
-  switch (key)
-  {
-    /* Exit on escape key press */
-    case '\x1B':
+    switch (key)
     {
-      exit(EXIT_SUCCESS);
-      break;
+        /* Exit on escape key press */
+        case '\x1B':
+            {
+            exit(EXIT_SUCCESS);
+            break;
+        }
     }
-  }
 }
 
 void display(void)
@@ -86,10 +89,10 @@ void display(void)
 
     glColor3f(0.0, 1.0, 0.0);
     glBegin(GL_QUADS);
-        glVertex3f(10.0, 10.0, 0.0);
-        glVertex3f(10.0, WINDOW_HEIGHT - 10, 0.0);
-        glVertex3f(WINDOW_WIDTH - 10, WINDOW_HEIGHT - 10, 0.0);
-        glVertex3f(WINDOW_WIDTH - 10, 10.0, 0.0);
+    glVertex3f(10.0, 10.0, 0.0);
+    glVertex3f(10.0, WINDOW_HEIGHT - 10, 0.0);
+    glVertex3f(WINDOW_WIDTH - 10, WINDOW_HEIGHT - 10, 0.0);
+    glVertex3f(WINDOW_WIDTH - 10, 10.0, 0.0);
     glEnd();
 
     glEnable(GL_TEXTURE_2D);
@@ -98,14 +101,14 @@ void display(void)
 
     //glColor3f(1.0, 1.0, 1.0);
     glBegin(GL_QUADS);
-        glTexCoord2f(0.0, 0.0);
-        glVertex3f(10.0, WINDOW_HEIGHT - 10, 0.0); // top left
-        glTexCoord2f(1.0, 0.0);
-        glVertex3f(WINDOW_WIDTH - 10, WINDOW_HEIGHT - 10, 0.0); // top right
-        glTexCoord2f(1.0, 0.8);  // <<<<  TEST with non full texture
-        glVertex3f(WINDOW_WIDTH - 10, 10.0, 0.0);  // bottom right
-        glTexCoord2f(0.0, 0.8);  // <<<<  TEST with non full texture
-        glVertex3f(10.0, 10.0, 0.0); // bottom left
+    glTexCoord2f(0.0, 0.0);
+    glVertex3f(10.0, WINDOW_HEIGHT - 10, 0.0); // top left
+    glTexCoord2f(1.0, 0.0);
+    glVertex3f(WINDOW_WIDTH - 10, WINDOW_HEIGHT - 10, 0.0); // top right
+    glTexCoord2f(1.0, 0.8);  // <<<<  TEST with non full texture
+    glVertex3f(WINDOW_WIDTH - 10, 10.0, 0.0);  // bottom right
+    glTexCoord2f(0.0, 0.8);  // <<<<  TEST with non full texture
+    glVertex3f(10.0, 10.0, 0.0); // bottom left
     glEnd();
     glDisable(GL_TEXTURE_2D);
 
@@ -140,11 +143,10 @@ int main(int argc, char** argv)
     init();
 
     glutDisplayFunc(display);
-    glutReshapeFunc(reshape); 
+    glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
 
     glutMainLoop();
 
     return EXIT_SUCCESS;
 }
-
