5aaacf9edba1b59e33c572f9465f4c971dc39639
Mathieu Gouin <mgouin@gmail.com>
2022-05-04 14:59:59 -0400

    Update main.c

--- main.c
+++ main.c
@@ -55,8 +55,8 @@ void init(void)
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);  // linear filter for magnification
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);  // linear filter for minifying
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth,
                  checkImageHeight,
